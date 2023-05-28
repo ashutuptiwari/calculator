d11bedaaca9bc62999df3f099459ba2ed360e908
ashutuptiwari <ashutosh.tiwari.cd.ece22@itbhu.ac.in>
2023-05-28 19:26:26 +0530

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,49 @@ import "./App.css";
 
 function App() {
   const [nstring, SetString] = useState("");
+
+  const buttons = [
+    { value: "+", onClick: () => HandleClick("+") },
+    { value: "-", onClick: () => HandleClick("-") },
+    { value: "*", onClick: () => HandleClick("*") },
+    { value: "Del", onClick: () => HandleDel() },
+    { value: "1", onClick: () => HandleClick("1") },
+    { value: "2", onClick: () => HandleClick("2") },
+    { value: "3", onClick: () => HandleClick("3") },
+    { value: "%", onClick: () => HandleClick("%") },
+    { value: "4", onClick: () => HandleClick("4") },
+    { value: "5", onClick: () => HandleClick("5") },
+    { value: "6", onClick: () => HandleClick("6") },
+    { value: "^", onClick: () => HandleClick("**") },
+    { value: "7", onClick: () => HandleClick("7") },
+    { value: "8", onClick: () => HandleClick("8") },
+    { value: "9", onClick: () => HandleClick("9") },
+    { value: "/", onClick: () => HandleClick("/") },
+    { value: "0", onClick: () => HandleClick("0") },
+    { value: "AC", onClick: () => HandleClearClick() },
+    { value: "=", onClick: () => HandleEqualsClick() },
+    { value: ".", onClick: () => HandleClick(".") },
+  ];
+
   const HandleClick = (v: string) => {
     SetString(nstring + v);
     console.log(v);
   };
+
   const HandleClearClick = () => {
     SetString("");
   };
+
   useEffect(() => {
     console.log(nstring);
   }, [nstring]);
+
   const HandleEqualsClick = () => {
     try {
       let result = eval(nstring);
-      if (result.toString() == "Infinity") SetString("TERI MAA KI CHUT");
-      else if (result.toString() == "NaN") SetString("BHAG BHOSDHIKE");
+      if (result.toString() === "Infinity" || "-Infinity")
+        SetString("TERI MAA KI CHU");
+      else if (result.toString() === "NaN") SetString("BHAG BHOSDHIKE");
       else SetString(result.toString());
 
       console.log(result);
@@ -27,150 +55,63 @@ function App() {
       SetString("CHUTIYA HAI KYA?");
     }
   };
+
   const HandleDel = () => {
     SetString(nstring.slice(0, -1));
     console.log(nstring);
   };
+
   return (
     <div id="body">
-      <h1> EXPLICIT CALCULATO</h1>
+      <h1> EXPLICIT CALCULATOR</h1>
 
       <div id="container">
         <Screen nstring={nstring} />
 
         <div id="sub-cont">
-          <Button
-            bvalue="+"
-            onClick={() => {
-              HandleClick("+");
-            }}
-          />
-          <Button
-            bvalue="-"
-            onClick={() => {
-              HandleClick("-");
-            }}
-          />
-          <Button
-            bvalue="*"
-            onClick={() => {
-              HandleClick("*");
-            }}
-          />
-          <Button
-            bvalue="Del"
-            onClick={() => {
-              HandleDel();
-            }}
-          />
+          {buttons.slice(0, 4).map((button, index) => (
+            <Button
+              key={index}
+              bvalue={button.value}
+              onClick={button.onClick}
+            />
+          ))}
         </div>
-
         <div id="sub-cont">
-          <Button
-            bvalue="1"
-            onClick={() => {
-              HandleClick("1");
-            }}
-          />
-          <Button
-            bvalue="2"
-            onClick={() => {
-              HandleClick("2");
-            }}
-          />
-          <Button
-            bvalue="3"
-            onClick={() => {
-              HandleClick("3");
-            }}
-          />
-          <Button
-            bvalue="%"
-            onClick={() => {
-              HandleClick("%");
-            }}
-          />
+          {buttons.slice(4, 8).map((button, index) => (
+            <Button
+              key={index}
+              bvalue={button.value}
+              onClick={button.onClick}
+            />
+          ))}
         </div>
-
         <div id="sub-cont">
-          <Button
-            bvalue="4"
-            onClick={() => {
-              HandleClick("4");
-            }}
-          />
-          <Button
-            bvalue="5"
-            onClick={() => {
-              HandleClick("5");
-            }}
-          />
-          <Button
-            bvalue="6"
-            onClick={() => {
-              HandleClick("6");
-            }}
-          />
-          <Button
-            bvalue="^"
-            onClick={() => {
-              HandleClick("**");
-            }}
-          />
+          {buttons.slice(8, 12).map((button, index) => (
+            <Button
+              key={index}
+              bvalue={button.value}
+              onClick={button.onClick}
+            />
+          ))}
         </div>
-
         <div id="sub-cont">
-          <Button
-            bvalue="7"
-            onClick={() => {
-              HandleClick("7");
-            }}
-          />
-          <Button
-            bvalue="8"
-            onClick={() => {
-              HandleClick("8");
-            }}
-          />
-          <Button
-            bvalue="9"
-            onClick={() => {
-              HandleClick("9");
-            }}
-          />
-          <Button
-            bvalue="/"
-            onClick={() => {
-              HandleClick("/");
-            }}
-          />
+          {buttons.slice(12, 16).map((button, index) => (
+            <Button
+              key={index}
+              bvalue={button.value}
+              onClick={button.onClick}
+            />
+          ))}
         </div>
-
         <div id="sub-cont">
-          <Button
-            bvalue="0"
-            onClick={() => {
-              HandleClick("0");
-            }}
-          />
-          <Button
-            bvalue="AC"
-            onClick={() => {
-              HandleClearClick();
-            }}
-          />
-          <Button
-            bvalue="="
-            onClick={() => {
-              HandleEqualsClick();
-            }}
-          />
-          <Button
-            bvalue="."
-            onClick={() => {
-              HandleClick(".");
-            }}
-          />
+          {buttons.slice(16, 20).map((button, index) => (
+            <Button
+              key={index}
+              bvalue={button.value}
+              onClick={button.onClick}
+            />
+          ))}
         </div>
       </div>
     </div>
